2a6231f592775005764cac33e90d401d4a994e5b
Tharusha Induwara <tharusha.induwar@gmail.com>
2021-08-17 20:58:03 +0530

    Begin coding on JS part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("Script loaded!");
+
+var userInput = document.getElementById("work");
+var inputText = userInput.innerHTML;
+
+var btn = document.getElementById("add-btn");
+
+btn.onmouseover = function(){
+    btn.classList.add("btn-hover");
+}
+btn.onmouseleave = function(){
+    btn.classList.remove("btn-hover");
+}
+btn.onmousedown = function(){
+    btn.classList.add("btn-click");
+    console.log(userInput.value);
+}
+btn.onmouseup = function(){
+    btn.classList.remove("btn-click");
+}
