32019c18d3c8f8d8684d8df7f4648f7b0425bb24
Tharusha Induwara <tharusha.induwar@gmail.com>
2021-08-17 22:38:32 +0530

    Added fonts for footer

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var inputText = userInput.innerHTML;
 
 var btn = document.getElementById("add-btn");
 
+var list = document.getElementById("middle-list");
+
 btn.onmouseover = function(){
     btn.classList.add("btn-hover");
 }
@@ -17,4 +19,14 @@ btn.onmousedown = function(){
 }
 btn.onmouseup = function(){
     btn.classList.remove("btn-click");
+}
+btn.onclick = function(){
+    var newItem = document.createElement("li");
+    var textNode = document.createTextNode(userInput.value);
+    newItem.appendChild(textNode);
+
+    list.appendChild(newItem);
+    console.log(list.childElementCount);
+
+    userInput.value="";
 }
